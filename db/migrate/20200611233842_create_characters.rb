340366ff0036625f0e554bfb97754429ac2352f9
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-11 23:39:56 +0000

    Automatically backed up by Learn

--- db/migrate/20200611233842_create_characters.rb
+++ db/migrate/20200611233842_create_characters.rb
@@ -1,8 +1,9 @@
 class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :actors do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
     end
   end
 end
