b0120232f7ac84341e7be37c63d327eb3c8a2aa5
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-11 23:52:56 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,7 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
   
   def list_roles
-    result = self.characters.map{|character| character && character.show}
+    result = self.characters.map{|character| character + character.show}
     binding.pry
   end
   
