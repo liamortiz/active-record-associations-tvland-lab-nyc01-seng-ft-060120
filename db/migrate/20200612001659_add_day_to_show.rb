5e7ad21bf9bd6aff9f8430af22bc3c0114dc72ee
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-12 00:17:44 +0000

    Automatically backed up by Learn

--- db/migrate/20200612001659_add_day_to_show.rb
+++ db/migrate/20200612001659_add_day_to_show.rb
@@ -1,5 +1,6 @@
 class AddDayToShow < ActiveRecord::Migration[6.0]
   def change
-    add_column(:shows, :day, :datetime)
+    add_column(:shows, :day, :string)
+    add_column(:shows, :seasons, :string)
   end
 end
